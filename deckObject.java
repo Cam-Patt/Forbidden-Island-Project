b91414a5fab4d0e590fcd218ba11259483490e13
simpissa <91391384+simpissa@users.noreply.github.com>
2021-11-08 13:27:14 -0600

    Update deckObject.java

--- deckObject.java
+++ deckObject.java
@@ -1,13 +1,28 @@
+
+import java.util.Collections;
 import java.util.Stack;
 
 public class deckObject{
-  private int numCards;
-  private Stack<cardObject> deck = new Stack<>();
-  
-  public deckObject(){
-    
-  }
-  public cardObject nextCard() {
-    return deck.pop();
-  }
+	private int numCards;
+	private Stack<cardObject> deck = new Stack<>();
+	private Stack<cardObject> discarded = new Stack<>();
+	
+	public deckObject(){
+		
+	}
+	public cardObject nextCard() {
+		cardObject card = deck.pop();
+		discarded.push(card);
+		return card;
+	}
+	public void remove(cardObject c) {
+		if(deck.contains(c)) {
+			deck.remove(deck.indexOf(c));
+		}
+	}
+	public void shuffle() {
+		Collections.shuffle(discarded);
+		deck.addAll(discarded);
+		discarded.clear();
+	}
 }
