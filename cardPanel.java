1ce1bafa1765d54fd631ce756a0dbeced3fac47c
simpissa <91391384+simpissa@users.noreply.github.com>
2021-11-09 13:41:50 -0600

    Update cardPanel.java

--- cardPanel.java
+++ cardPanel.java
@@ -1,13 +1,43 @@
 
-import java.awt.*;
-import javax.swing.*;
-public class cardPanel extends JPanel{
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class cardPanel extends JPanel implements MouseListener{
 	public cardPanel() {
 		setVisible(true);
+		addMouseListener(this);
+		JButton backButton = new JButton("Back");
+		backButton.setSize(100, 30);
+		backButton.setLocation(250, 250);
+		
+		
 	}	
 	public void paint(Graphics g) {
 		super.paintComponent(g);
-		g.drawString();
+		
 	}
-	
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		
+	}
+
 }
