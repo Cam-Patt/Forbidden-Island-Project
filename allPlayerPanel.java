844870b7dd994dd2ab989cbd59c4dcdf30afd19f
simpissa <91391384+simpissa@users.noreply.github.com>
2021-11-09 13:07:14 -0600

    Update allPlayerPanel.java

--- allPlayerPanel.java
+++ allPlayerPanel.java
@@ -1,18 +1,18 @@
-import java.awt.;
+import java.awt.*;
 import javax.swing.*;
-public class allPlayerPanel extends JPanel{
-  private ArrayList<playerObject> playerObjects;
-  private ArrayList<playerPanel> playerPanels;
-  public allPlayerPanel(ArrayList<playerObject> p){
-  
-  }
-  public void paint(Graphics g){
-  
-  }
-  public void turn(int playerNum){
-  //draws a boarder around the player
-  }
-  public void highlight(ArrayList<playerObject> list){
-   //draws a boarder around all players in list
-  }
+public class allPlayerPanel extends JPanel {
+    private ArrayList<playerObject> playerObjects;
+    private ArrayList<playerPanel> playerPanels;
+    public allPlayerPanel(ArrayList<playerObject> p) {
+      
+    } 
+    public void paint(Graphics g) {
+      
+    }
+    public void turn(int playerNum) { //draws a border around the player
+      
+    }
+    public void highlight(ArrayList<playerObject> list) { //draws a border around all players in list
+      
+    }
 }
