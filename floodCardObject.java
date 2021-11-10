88c2474156be94ad0a597417b7883f2e64990ed3
k1525229 <92116703+k1525229@users.noreply.github.com>
2021-11-10 10:01:38 -0600

    Update floodCardObject.java

--- floodCardObject.java
+++ floodCardObject.java
@@ -1,4 +1,7 @@
-public class floodCardObject{
-  public floodCardObject(){
-  }
+import java.awt.image.*;
+public class floodCardObject extends cardObject{
+	public floodCardObject(BufferedImage i, String s) {
+		super(i, s);
+	}
+	
 }
