439417ff0f85644bff52c8161a63e17928e2052d
simpissa <91391384+simpissa@users.noreply.github.com>
2021-11-10 13:40:16 -0600

    Update playerPanel.java

--- playerPanel.java
+++ playerPanel.java
@@ -1,10 +1,44 @@
-import java.awt.*;
-import java.swing.*;
-public class playerPanel extends JPanel{
-  public playerPanel(playerObject p){
-  
-  }
-  public void paint(Graphics g){
-  
-  }
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
+
+public class playerPanel extends JPanel {
+	private String playerRole;
+	private BufferedImage Navigator, Messenger, Explorer, Pilot, Engineer, Diver;
+	public playerPanel(playerObject p) {
+		playerRole = p.getRole();
+		try {
+			Navigator = ImageIO.read(playerPanel.class.getResource("/Images/Navigator.PNG"));
+			Messenger = ImageIO.read(playerPanel.class.getResource("/Images/Messenger.PNG"));
+			Explorer = ImageIO.read(playerPanel.class.getResource("/Images/Explorer.PNG"));
+			Pilot = ImageIO.read(playerPanel.class.getResource("/Images/Pilot.PNG"));
+			Engineer = ImageIO.read(playerPanel.class.getResource("/Images/Engineer.PNG"));
+			Diver = ImageIO.read(playerPanel.class.getResource("/Images/Diver.PNG"));
+			
+		}
+		catch (Exception E){
+			System.out.println("Roles not found");
+		}
+	}
+	public void paint(Graphics g) {
+		super.paintComponent(g);
+		switch (playerRole) {
+		case "Navigator":
+			break;
+		case "Messenger":
+			break;
+		case "Explorer":
+			break;
+		case "Pilot": 
+			break;
+		case "Engineer": 
+			break;
+		case "Diver": 
+			break;
+		default: 
+			System.out.println("Player Role Not Found");
+		}
+	}
 }
