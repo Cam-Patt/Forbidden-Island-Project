0cc9e954b81b727448e1fc97f885beadeef81ef9
k1525229 <92116703+k1525229@users.noreply.github.com>
2021-11-10 10:03:26 -0600

    Update waterRiseCardObject.java

--- waterRiseCardObject.java
+++ waterRiseCardObject.java
@@ -1,5 +1,6 @@
-public class waterRiseCardObject{
-  public waterRiseCardObject(){
-  
-  }
+import java.awt.image.*;
+public class waterRiseCardObject extends treasureCardObject{
+	public waterRiseCardObject(BufferedImage i, String s) {
+		super(i,s);
+	}
 }
