9cb518b75d7ddcb11a79117951dd11629e6ca9c0
simpissa <91391384+simpissa@users.noreply.github.com>
2021-11-10 13:11:53 -0600

    Update playerObject.java

--- playerObject.java
+++ playerObject.java
@@ -2,9 +2,11 @@
 public class playerObject{
   private String name;
   private boolean[] abilities;
+  private String role;
   private int playerNum;
   private Color playerColor;
   public playerObject(int num) {
+  
   
   }
 }
