2c75a35c20d22c8dad0e23b9c34518df25fcbc03
k1525229 <92116703+k1525229@users.noreply.github.com>
2021-11-10 10:02:16 -0600

    Update specialActionCardObject.java

--- specialActionCardObject.java
+++ specialActionCardObject.java
@@ -1,5 +1,25 @@
-public class specialActionCard{
-  public specialActionCard(){
-  
-  }
+import java.awt.image.*;
+public class specialActionCardObject extends treasureCardObject{
+	private String type;
+	public specialActionCardObject(BufferedImage i, String t, String type) {
+		super(i,t);
+		this.type = type;
+	}
+	public String getType() {
+		return type;
+	}
+	public boolean isShoreUP() {
+		if(type.equals("shoreUp")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public boolean isHelicopter() {
+		if(type.equals("helicoper")) {
+			return true;
+		}else {
+			return false;
+		}
+	}
 }
