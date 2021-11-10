013cb8ef49f19872958445d9c8ffc80557ff140e
k1525229 <92116703+k1525229@users.noreply.github.com>
2021-11-10 10:01:10 -0600

    Update cardObject.java

--- cardObject.java
+++ cardObject.java
@@ -1,6 +1,18 @@
-public class cardObject{
-  
-  public cardObject(){
-  
-  }
+import java.awt.image.*;
+public class cardObject {
+	private BufferedImage cardFace;
+	private String title;
+	public cardObject(BufferedImage i, String s) {
+		cardFace = i;
+		title = s;
+	}
+	public BufferedImage getFace() {
+		return cardFace;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public boolean isEqual(cardObject c) {
+		return(c.getTitle().equals(this.title));
+	}
 }
