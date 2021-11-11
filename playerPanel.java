36a1107cc15365f3c6e59126f9aa77481fb38bfc
simpissa <91391384+simpissa@users.noreply.github.com>
2021-11-11 13:18:53 -0600

    Update playerPanel.java

--- playerPanel.java
+++ playerPanel.java
@@ -23,19 +23,24 @@ public class playerPanel extends JPanel {
 		}
 	}
 	public void paint(Graphics g) {
-		super.paintComponent(g);
 		switch (playerRole) {
 		case "Navigator":
+			g.drawImage(Navigator, 0, 0, 250, 180, null);
 			break;
 		case "Messenger":
+			g.drawImage(Messenger, 0, 0, 250, 180, null);
 			break;
 		case "Explorer":
+			g.drawImage(Explorer, 0, 0, 250, 180, null);
 			break;
 		case "Pilot": 
+			g.drawImage(Pilot, 0, 0, 250, 180, null);
 			break;
 		case "Engineer": 
+			g.drawImage(Engineer, 0, 0, 250, 180, null);
 			break;
 		case "Diver": 
+			g.drawImage(Diver, 0, 0, 250, 180, null);
 			break;
 		default: 
 			System.out.println("Player Role Not Found");
