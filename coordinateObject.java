3866310d43b5771982515529172d13277122339a
Sulaiman Mulla <93167114+Sulaiman-1@users.noreply.github.com>
2021-11-15 13:41:10 -0600

    Update coordinateObject.java

--- coordinateObject.java
+++ coordinateObject.java
@@ -1,5 +1,38 @@
-public class coordinateObject{
-  public coordinateObject(){
-  
-  }
+import java.util.ArrayList;
+import java.util.TreeMap;
+public class coordinate 
+{
+	private ArrayList <TileObject> tiles = new ArrayList<>();
+	private TreeMap<String, String> coordinates = new TreeMap<>();
+	
+	public coordinate(ArrayList<TileObject> t)
+	{
+		tiles = t;
+		
+		coordinates.put("A1", null);
+		coordinates.put("A2", null);
+		coordinates.put("B1", null);
+		coordinates.put("B2", null);
+		coordinates.put("B3", null);
+		coordinates.put("B4", null);
+		coordinates.put("C1", null);
+		coordinates.put("C2", null);
+		coordinates.put("C3", null);
+		coordinates.put("C4", null);
+		coordinates.put("C5", null);
+		coordinates.put("C6", null);
+		coordinates.put("D1", null);
+		coordinates.put("D2", null);
+		coordinates.put("D3", null);
+		coordinates.put("D4", null);
+		coordinates.put("D5", null);
+		coordinates.put("D6", null);
+		coordinates.put("E1", null);
+		coordinates.put("E2", null);
+		coordinates.put("E3", null);
+		coordinates.put("E4", null);
+		coordinates.put("F1", null);
+		coordinates.put("F2", null);
+
+	}
 }
