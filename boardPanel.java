3eb5a4a38ed431e6d33de7126cc0396434e12fd2
k1525229 <92116703+k1525229@users.noreply.github.com>
2021-11-15 13:27:18 -0600

    Update boardPanel.java
    this is clearly not finished, but I will get it done at my house

--- boardPanel.java
+++ boardPanel.java
@@ -1,7 +1,14 @@
 import java.awt.*;
-import java.swing.*;
-public class boardPanel extend JPanel{
-  public boardPanel(){
-  
-  }
+import javax.swing.*;
+public class boardPanel extends JPanel{
+	private boardObject board;
+	public boardPanel(boardObject b) {
+		board = b;
+	}
+	public void paint(Graphics g) {
+		TileObject[][] b = board.getBoard();
+		GridLayout lay = new GridLayout(6,6);
+		setLayout(lay);
+		add(new tilePanel(b[0][0]),0,0);
+	}
 }
