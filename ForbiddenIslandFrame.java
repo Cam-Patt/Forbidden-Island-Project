9143924bd33f9da2b27014f8c6be54522fddd400
you should be working <92116703+k1525229@users.noreply.github.com>
2021-11-18 13:27:24 -0600

    Update ForbiddenIslandFrame.java

--- ForbiddenIslandFrame.java
+++ ForbiddenIslandFrame.java
@@ -1,6 +1,7 @@
 //here is the frame that we will use
 import javax.swing.*;
 import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Random;
 public class ForbiddenIslandFrame extends JFrame{
@@ -17,34 +18,42 @@ public class ForbiddenIslandFrame extends JFrame{
 		while(!(title.start())) {
 			//this is empty so the code runs til the start is set
 		}
+		remove(title);
+		try {
+			boardPanel board = new boardPanel(new boardObject());
+			add(board);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-	public ArrayList<playerObject> makePlayers(){
+	public ArrayList<playerObject> makePlayers() throws IOException{
 		ArrayList<playerObject> fullList = new ArrayList<playerObject>();
 		//makes a list of all possible characters
 		playerObject explorer;
 		boolean[] abilities = {true,false,false,false,false,false};
 		Color c = new Color(2);
-		explorer = new playerObject("Explorer",abilities, c);
+		explorer = new playerObject("Explorer",abilities, c, new TileObject("Copper Gate", "Explorer", "Copper Gate" + "@2x.png", "Copper Gate" + "_flood@2x.png"));
 		playerObject pilot;
 		boolean[] abilitie = {false,true,false,false,false,false};
 		c = new Color(3);
-		pilot = new playerObject("Pilot",abilitie, c);
+		pilot = new playerObject("Pilot",abilitie, c, new TileObject("Fools' Landing" , "Pilot", "Fools' Landing" + "@2x.png", "Fools' Landing" + "_flood@2x.png"));
 		playerObject navigator;
 		boolean[] ab = {false,false,true,false,false,false};
 		c = new Color(4);
-		navigator= new playerObject("Navigator", ab, c);
+		navigator= new playerObject("Navigator", ab, c, new TileObject("Gold Gate", "Navigator", "Gold Gate" + "@2x.png", "Gold Gate" + "_flood@2x.png"));
 		playerObject diver;
 		boolean[] b = {false,false,false,true,false,false};
 		c = new Color(5);
-		diver = new playerObject("Diver", b, c);
+		diver = new playerObject("Diver", b, c,new TileObject("Iron Gate", "Diver", "Iron Gate" + "@2x.png", "Iron Gate" + "_flood@2x.png"));
 		playerObject engineer;
 		boolean[] a = {false,false,false,false,true,false};
 		c = new Color(6);
-		engineer = new playerObject("Engineer",a,c);
+		engineer = new playerObject("Engineer",a,c, new TileObject("Bronze Gate", "Engineer", "Bronze Gate" + "@2x.png", "Bronze Gate" + "_flood@2x.png"));
 		playerObject messenger;
 		boolean[] list = {false,false,false,false,false,true};
 		c = new Color(7);
-		messenger = new playerObject("Messenger", list,c);
+		messenger = new playerObject("Messenger", list,c, new TileObject("Silver Gate", "Messenger", "Silver Gate" + "@2x.png", "Silver Gate" + "_flood@2x.png"));
 		
 		fullList.add(explorer);
 		fullList.add(pilot);
