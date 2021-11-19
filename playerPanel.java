f58329957f0dd63c6bacc1f04561b2da740c5032
simpissa <91391384+simpissa@users.noreply.github.com>
2021-11-19 13:42:33 -0600

    Update playerPanel.java

--- playerPanel.java
+++ playerPanel.java
@@ -1,75 +1,97 @@
+package Test;
 
 import javax.imageio.ImageIO;
-import javax.swing.JPanel;
+
 import java.awt.image.BufferedImage;
-import java.awt.Graphics;
+
+import java.awt.*;
+import javax.swing.*;
 
 public class playerPanel extends JPanel {
-	private playerObject player;
 	private String playerRole;
-	private BufferedImage Navigator, Messenger, Explorer, Pilot, Engineer, Diver;
-	public playerPanel(playerObject p) {
-		player = p;
-		playerRole = player.getRole();
-		try {
-			Navigator = ImageIO.read(getClass().getResource("/Images/Navigator.PNG"));
-			Messenger = ImageIO.read(getClass().getResource("/Images/Messenger.PNG"));
-			Explorer = ImageIO.read(getClass().getResource("/Images/Explorer.PNG"));
-			Pilot = ImageIO.read(getClass().getResource("/Images/Pilot.PNG"));
-			Engineer = ImageIO.read(getClass().getResource("/Images/Engineer.PNG"));
-			Diver = ImageIO.read(getClass().getResource("/Images/Diver.PNG"));
-			
-		}
-		catch (Exception E){
-			System.out.println("Roles not found");
-		}
+	private BufferedImage Navigator, Messenger, Explorer, Pilot, Engineer, Diver, Card_Crystal_of_Fire, Card_Earth_Stone, Card_Oceans_Chalice, Card_Statue_of_the_Wind, Card_Helicopter, Card_Sand_Bag;
+	//public playerPanel(playerObject p) {
+	public playerPanel() {
+		//playerRole = p.getRole();
+		//playerRole = "Pilot";
+//		try {
+//			Navigator = ImageIO.read(getClass().getResource("/Images/Navigator.PNG"));
+//			Messenger = ImageIO.read(getClass().getResource("/Images/Messenger.PNG"));
+//			Explorer = ImageIO.read(getClass().getResource("/Images/Explorer.PNG"));
+//			Pilot = ImageIO.read(getClass().getResource("/Images/Pilot.PNG"));
+//			Engineer = ImageIO.read(getClass().getResource("/Images/Engineer.PNG"));
+//			Diver = ImageIO.read(getClass().getResource("/Images/Diver.PNG"));
+//			Card_Crystal_of_Fire = ImageIO.read(getClass().getResource("/Images/Card_Crystal_of_Fire.png"));
+//			Card_Earth_Stone = ImageIO.read(getClass().getResource("/Images/Card_Earth_Stone.png"));
+//			Card_Oceans_Chalice = ImageIO.read(getClass().getResource("/Images/Card_Oceans_Chalice.png"));
+//			Card_Statue_of_the_Wind = ImageIO.read(getClass().getResource("/Images/Card_Statue_of_the_Wind.png"));
+//			Card_Helicopter = ImageIO.read(getClass().getResource("/Images/Card_Helicopter.png"));
+//			Card_Sand_Bag = ImageIO.read(getClass().getResource("/Images/Card_Sand_Bag.png"));
+//		}
+//		catch (Exception E){
+//			System.out.println("Roles not found");
+//		}
+
+		//add(new JButton("worked"));
+		//setSize(200, 200);
+		//setVisible(true);
+		System.out.println(getWidth()+" "+getHeight());
 	}
+	//paint panel not being called
 	public void paint(Graphics g) {
-		switch (playerRole) {
-		case "Navigator":
-			g.drawImage(Navigator, 0, 0, 250, 180, null);
-			break;
-		case "Messenger":
-			g.drawImage(Messenger, 0, 0, 250, 180, null);
-			break;
-		case "Explorer":
-			g.drawImage(Explorer, 0, 0, 250, 180, null);
-			break;
-		case "Pilot": 
-			g.drawImage(Pilot, 0, 0, 250, 180, null);
-			break;
-		case "Engineer": 
-			g.drawImage(Engineer, 0, 0, 250, 180, null);
-			break;
-		case "Diver": 
-			g.drawImage(Diver, 0, 0, 250, 180, null);
-			break;
-		default: 
-			System.out.println("Player Role Not Found");
-		}
-		//this is a temporary replacement for calling the cardPanel multiple times for each player
-		for(int i=0;i<player.getDeck().size();i++) {
-			switch (player.getDeck().get(i)) {
-			case "Fire":
-				g.drawImage(Card_Crystal_of_Fire, 100, 100, 100+i*120, 100, null);
-				break;
-			case "Stone":
-				g.drawImage(Card_Earth_Stone, 100, 100, 100+i*120, 100, null);
-				break;
-			case "Chalice":
-				g.drawImage(Card_Oceans_Chalice, 100, 100, 100+i*120, 100, null);
-				break;
-			case "Wind":
-				g.drawImage(Card_Statue_of_the_Wind, 100, 100, 100+i*120, 100, null);
-				break;
-			case "Helicopter":
-				g.drawImage(Card_Helicopter, 100, 100, 100+i*120, 100, null);
-				break;
-			case "Sandbag":
-				g.drawImage(Card_Sand_Bag, 100, 100, 100+i*120, 100, null);
-				break;
-			default:
-				System.out.println("Error with player cards");
-		}
+		
+		//super.paintComponent(g);
+		g.setColor(Color.RED);
+		//g.fillRect(0, 0, 300, 234);
+		System.out.println(getWidth()+" "+getHeight());
+		//g.fillRect(0, 0, getWidth(), getHeight());
+		
+		
+//		switch (playerRole) {
+//		case "Navigator":
+//			g.drawImage(Navigator, 0, 0, 250, 180, null);
+//			break;
+//		case "Messenger":
+//			g.drawImage(Messenger, 0, 0, 250, 180, null);
+//			break;
+//		case "Explorer":
+//			g.drawImage(Explorer, 0, 0, 250, 180, null);
+//			break;
+//		case "Pilot": 
+//			g.drawImage(Pilot, 0, 0, 250, 180, null);
+//			break;
+//		case "Engineer": 
+//			g.drawImage(Engineer, 0, 0, 250, 180, null);
+//			break;
+//		case "Diver": 
+//			g.drawImage(Diver, 0, 0, 250, 180, null);
+//			break;
+//		default: 
+//			System.out.println("Player Role Not Found");
+//		}
+		
+//		for(int i=0;i<p.getDeck().size();i++) {
+//			switch (p.getDeck().get(i)) {
+//			case "Fire":
+//				g.drawImage(Card_Crystal_of_Fire, 100, 100, 100+i*120, 100, null);
+//				break;
+//			case "Stone":
+//				g.drawImage(Card_Earth_Stone, 100, 100, 100+i*120, 100, null);
+//				break;
+//			case "Chalice":
+//				g.drawImage(Card_Oceans_Chalice, 100, 100, 100+i*120, 100, null);
+//				break;
+//			case "Wind":
+//				g.drawImage(Card_Statue_of_the_Wind, 100, 100, 100+i*120, 100, null);
+//				break;
+//			case "Helicopter":
+//				g.drawImage(Card_Helicopter, 100, 100, 100+i*120, 100, null);
+//				break;
+//			case "Sandbag":
+//				g.drawImage(Card_Sand_Bag, 100, 100, 100+i*120, 100, null);
+//				break;
+//			default:
+//				System.out.println("Error with player cards");
+//		}
 	}
 }
