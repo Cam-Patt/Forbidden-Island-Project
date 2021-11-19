baa6f6a7e8ddd926bdeda78f458400bc346360a3
simpissa <91391384+simpissa@users.noreply.github.com>
2021-11-19 12:57:32 -0600

    Update playerPanel.java

--- playerPanel.java
+++ playerPanel.java
@@ -5,10 +5,12 @@ import java.awt.image.BufferedImage;
 import java.awt.Graphics;
 
 public class playerPanel extends JPanel {
+	private playerObject player;
 	private String playerRole;
 	private BufferedImage Navigator, Messenger, Explorer, Pilot, Engineer, Diver;
 	public playerPanel(playerObject p) {
-		playerRole = p.getRole();
+		player = p;
+		playerRole = player.getRole();
 		try {
 			Navigator = ImageIO.read(getClass().getResource("/Images/Navigator.PNG"));
 			Messenger = ImageIO.read(getClass().getResource("/Images/Messenger.PNG"));
@@ -44,6 +46,29 @@ public class playerPanel extends JPanel {
 			break;
 		default: 
 			System.out.println("Player Role Not Found");
+		}
+		for(int i=0;i<player.getDeck().size();i++) {
+			switch (player.getDeck().get(i)) {
+			case "Fire":
+				g.drawImage(Card_Crystal_of_Fire, 100, 100, 100+i*120, 100, null);
+				break;
+			case "Stone":
+				g.drawImage(Card_Earth_Stone, 100, 100, 100+i*120, 100, null);
+				break;
+			case "Chalice":
+				g.drawImage(Card_Oceans_Chalice, 100, 100, 100+i*120, 100, null);
+				break;
+			case "Wind":
+				g.drawImage(Card_Statue_of_the_Wind, 100, 100, 100+i*120, 100, null);
+				break;
+			case "Helicopter":
+				g.drawImage(Card_Helicopter, 100, 100, 100+i*120, 100, null);
+				break;
+			case "Sandbag":
+				g.drawImage(Card_Sand_Bag, 100, 100, 100+i*120, 100, null);
+				break;
+			default:
+				System.out.println("Error with player cards");
 		}
 	}
 }
